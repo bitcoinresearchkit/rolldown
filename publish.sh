4dcf5a33a65e22f2333926716145bae11087295e
nym21 <expat-epochs0i@icloud.com>
2025-11-19 12:26:49 +0100

    published

---
+++ publish.sh
@@ -0,0 +1,52 @@
+cd crates/string_wizard
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_std_utils
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_utils
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_error
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_ecmascript
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_fs
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_rstr
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_sourcemap
+cargo publish --allow-dirty --no-verify
+cd ../string_wizard
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_common
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_debug_action
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_debug
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_ecmascript_utils
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_resolver
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_devtools_action
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_devtools
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_plugin
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_plugin_data_uri
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_plugin_hmr
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_tracing
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_plugin_chunk_import_map
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_watcher
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_plugin_oxc_runtime
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_dev_common
+cargo publish --allow-dirty --no-verify
+cd ../rolldown_workspace
+cargo publish --allow-dirty --no-verify
+cd ../rolldown
+cargo publish --allow-dirty --no-verify
